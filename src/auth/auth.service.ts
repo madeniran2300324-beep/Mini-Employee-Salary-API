d058d493d33387275ec013981108711e724e6e21
Covenant University <madeniran.2300324@stu.cu.edu.ng>
2026-08-18 16:17:13 +0100

    Phase 2: implement AuthService.login() with JWT signing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, UnauthorizedException } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { RegisterDto } from "./dto/register.dto";
 import * as bcrypt from 'bcrypt'
+import { LoginDto } from "./dto/login.dto";
+import { JwtService } from "@nestjs/jwt";
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService, private jwt: JwtService) {}
     async register(data : RegisterDto){
         const existingEmployer = await this.prisma.employer.findUnique({ where: { email: data.email } });
         if (existingEmployer){
@@ -14,7 +16,19 @@ export class AuthService {
         const employer = await this.prisma.employer.create({ data: { firstName:data.firstName, lastName: data.lastName, email:data.email, passwordHash:hashedPassword}});
         const { passwordHash, ...result } = employer;
         return result
-        
+    }
+    async login(data : LoginDto){
+        const employer = await this.prisma.employer.findUnique({ where: { email: data.email }});
+        if (!employer) {
+            throw new UnauthorizedException('Invalid Credentials');
+        }  
+        const isPasswordValid = await bcrypt.compare(data.password, employer.passwordHash);
+        if (!isPasswordValid){
+            throw new UnauthorizedException('Invalid Credentials');
+        }
+        const payload = { sub: employer.id };
+        const token = this.jwt.sign(payload)
+        return {access_token: token}
     }
 }
 
