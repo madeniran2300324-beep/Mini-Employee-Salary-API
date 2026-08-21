17e28d1767690c5f358ec987d8ccc092a27a3f48
Covenant University <madeniran.2300324@stu.cu.edu.ng>
2026-08-21 11:29:51 +0100

    Phase 3: correct EmployeeStatus values to ACTIVE, ON_LEAVE, SUSPENDED

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,40 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class EmployeesService {
-  private employees: {
-    id: number; //properties and types should nenevr be in quotes
-    name: string;
-    role: string;
-    baseSalary: number;
-  }[] = []; //this closes a type definition
-  private idCounter = 1;
 
-  addEmployee(name: string, role: string, baseSalary: number) {
-    const employee = {
-      id: this.idCounter,
-      name: name,
-      role: role,
-      baseSalary: baseSalary,
-    };
-    this.employees.push(employee);
-    this.idCounter++;
-    return employee;
-  }
-
-  getAllEmployees() {
-    return this.employees;
-  }
-
-  getEmployeePay(id: number) {
-    const employee = this.employees.find((emp) => emp.id === id);
-    if (!employee) return null;
-    const netPay = employee.baseSalary * 0.9;
-    return { name: employee.name, netPay };
-  }
-
-  deleteEmployee(id: number) {
-    this.employees = this.employees.filter((emp) => emp.id !== id);
-    return 'Employee deleted';
-  }
 }
