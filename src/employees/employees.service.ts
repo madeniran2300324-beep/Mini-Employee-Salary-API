e9f600f3bbb4b6f784c54a8c24d771494ea21d32
Covenant University <madeniran.2300324@stu.cu.edu.ng>
2026-08-24 11:32:22 +0100

    Phase 3: implement EmployeesController with nested routes and dual ownership guards

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
 @Injectable()
 export class EmployeesService {
@@ -24,6 +25,23 @@ export class EmployeesService {
   }
   async findOne(employeeId: string) {
     const employee = await this.prisma.employee.findUnique({ where: { id: employeeId } })
+    return employee;
+  }
+  async update(employeeId: string, data: UpdateEmployeeDto) {
+    const employee = await this.prisma.employee.update({ where: { id: employeeId}, 
+      data: {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        employeeNumber: data.employeeNumber,
+        jobTitle: data.jobTitle,
+        status: data.status
+      }
+    });
+    return employee;
+  }
+  async delete(employeeId: string){
+    const employee = await this.prisma.employee.delete({ where: {id: employeeId } })
     return employee;
   }
 
